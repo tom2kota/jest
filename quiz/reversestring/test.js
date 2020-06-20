9e84c344d880e671e1a437e39df3283faccb8acb
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-06-20 03:16:22 +0300

    Initial commit

---
+++ quiz/reversestring/test.js
@@ -0,0 +1,14 @@
+import {expect, test} from "@jest/globals";
+import reverse from "./index";
+
+test('Reverse function exists', () => {
+    expect(reverse).toBeDefined();
+});
+
+test('Reverse reverses a string', () => {
+    expect(reverse('abcd')).toEqual('dcba');
+});
+
+test('Reverse reverses a string', () => {
+    expect(reverse('  abcd')).toEqual('dcba  ');
+});
