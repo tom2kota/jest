3870f4d438b57ff809600d8b059ddb03d6cc2030
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-07-14 12:27:11 +0300

    Initial commit

---
+++ quiz/vowels1/test.js
@@ -0,0 +1,18 @@
+import vowels from "./index";
+import {expect, test} from "@jest/globals";
+
+test('Vowels is a function', () => {
+    expect(typeof vowels).toEqual('function');
+});
+
+test('returns the number of vowels used', () => {
+    expect(vowels('aeiou')).toEqual(5);
+});
+
+test('returns the number of vowels used', () => {
+    expect(vowels('abcdefghijklmnopqrstuvwxyz')).toEqual(5);
+});
+
+test('returns the number of vowels used', () => {
+    expect(vowels('bcdfghjkl')).toEqual(0);
+});
