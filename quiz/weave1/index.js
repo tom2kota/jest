0a9484388d559b63cea9a6010a8cfe5d6cf0f06e
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-07-14 12:28:21 +0300

    Initial commit

---
+++ quiz/weave1/index.js
@@ -0,0 +1,19 @@
+import Queue from "./queue"
+
+function weave(sourceOne, sourceTwo) {
+    const q = new Queue();
+
+    while (sourceOne.peek() || sourceTwo.peek()) {
+        if (sourceOne.peek()) {
+            q.add(sourceOne.remove());
+        }
+
+        if (sourceTwo.peek()) {
+            q.add(sourceTwo.remove());
+        }
+    }
+
+    return q;
+}
+
+export default weave;
