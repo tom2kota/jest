3c7a43ea6526ec421b088c378f126b031139bc23
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-07-14 12:13:55 +0300

    Initial commit

---
+++ quiz/midpoint1/index.js
@@ -0,0 +1,27 @@
+// export default (list) => {
+//
+//     let slow = list.getFirst();
+//     let fast = list.getFirst();
+//
+//     while (fast.next && fast.next.next) {
+//         slow = slow.next;
+//         fast = fast.next.next;
+//     }
+//
+//     return slow;
+//
+// }
+
+function midpoint(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
+
+module.exports = midpoint;
