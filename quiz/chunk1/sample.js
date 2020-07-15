4e63d799ce65a2e72758576bd8b5e63e136f4598
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-07-15 16:37:54 +0300

    Update files

--- quiz/chunk1/sample.js
+++ quiz/chunk1/sample.js
@@ -1,9 +1,11 @@
-export default (array, size) => {
+((array, size) => {
     const result = [];
     let index = 0;
     while (index < array.length) {
         result.push(array.slice(index, index + size));
         index += size;
     }
+    console.log(result)
     return result;
-}
+
+})([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)
